d9f44fa73d419ef0900e3c025d694602a8df61ac
bvrajugithub2016 <bvraju_sca@yahoo.com>
2022-11-30 14:22:24 +0530

    First Commit

---
+++ src/main/java/com/bvraju/demo/SpringBootWebForJenkinsDockerEx2Application.java
@@ -0,0 +1,21 @@
+package com.bvraju.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootWebForJenkinsDockerEx2Application {
+
+	@GetMapping("/hello")
+	public String welcome() {
+		return "Hello Mr. BVRaju, welcome to Spring Boot RESTFul API Web Application - for Jenkins Docker Integration";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebForJenkinsDockerEx2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bvraju/demo/SpringBootWebForJenkinsDockerEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvraju.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebForJenkinsDockerEx2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
